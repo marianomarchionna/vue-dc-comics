ec00e3c4c616ebe21ffa69476551b17e72383f33
Mariano <marianomarchionna8@gmail.com>
2021-09-21 16:12:02 +0200

    Jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="jumbotron">
+        <img src="@/assets/img/jumbotron.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Jumbotron'
+}
+</script>
+
+<style scoped lang="scss">
+.jumbotron{
+    height: 350px;
+    img{
+        height: 100%;
+        width: 100%;
+        object-position: top;
+        object-fit: cover;
+    };
+}
+</style>
